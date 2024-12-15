29123716006a7c46e85a4b3116d842e8bedde636
SOUFIANEOULAHCEN <soufiane.oulahcen@gmail.com>
2024-12-15 18:52:22 +0100

    modifier icons hover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import EyeIcon from "./EyeIcon";
 
 export default function Card({
@@ -5,13 +6,19 @@ export default function Card({
   description = "Strategic brilliance is at the core of the Akatsuki's operations. We craft meticulous plans, always two steps ahead, ensuring success in even the most dangerous of missions.",
   Icon = EyeIcon,
 }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="max-w-[400px] p-6 bg-[#1A1B26] rounded-lg shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-[#922C44] group">
+    <div 
+      className="max-w-[400px] p-6 bg-[#1A1B26] rounded-lg shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:bg-[#922C44]"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="mb-4">
         <Icon
-          color="#922C44"
+          color={isHovered ? "#fff" : "#922C44"}
           size={45}
-          className="transition-all duration-300 group-hover:[&>path]:stroke-[#fff] group-hover:[&>circle]:stroke-[#fff]"
+          className="transition-all duration-300"
         />
       </div>
       <div className="text-gray-100">
